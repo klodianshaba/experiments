c2e60efa1e1cf4573ecd4f04c9ccc2948e079bd3
klodian shaba <dev.klodian@gmail.com>
2024-05-14 11:26:51 +0200

    routes config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { permissionGuard } from './guards/permission.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.component').then(
+        c => c.DashboardComponent
+      ),
+    canActivate: [permissionGuard(1)],
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./pages/admin/admin.component').then(c => c.AdminComponent),
+    canActivate: [permissionGuard(5)],
+  },
+];
