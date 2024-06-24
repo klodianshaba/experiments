63aff8c906853a35a5f47672cb914280503a2a57
klodian shaba <dev.klodian@gmail.com>
2024-06-24 13:27:27 +0200

    empty i18n

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -35,9 +35,8 @@ export class DashboardComponent {
   percentAmount: number = 0;
   todoStore = inject(todoStore);
   user = 'Klodian Shaba';
-  title = 'Dashboard';
+  title = $localize`Dashboard`;
   constructor() {
-    $localize`${this.title}`;
     afterRender(() => {
       this.elementRef.nativeElement.scrollHeight;
     });
